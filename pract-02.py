52f1f5b89284f00930318f039b42d45dcb363bf2
warmMySoul <61860063+warmMySoul@users.noreply.github.com>
2021-01-12 01:07:49 +0300

    Update pract-02.py

--- pract-02.py
+++ pract-02.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+# Разница массивов.
 def AbsArrayDifference(A, B):
     A = np.asarray(A)
     B = np.asarray(B)
@@ -8,23 +9,32 @@ def AbsArrayDifference(A, B):
 
     return np.absolute(A - B)
 
-def HighestSumArray(arrayAmount, itemAmmount):
+# Генерация массивов.
+def AddMass(arrayAmount, itemAmmount)
     arrays = np.zeros((arrayAmount, itemAmmount))
-    
     
     for i in range(arrayAmount):
         arrays[i] = 10 * np.random.random(itemAmmount) - 5      
+    
+    return arrays[]
+
+# Наибольшая сумма массива.
+def HighestSumArray(arrayAmount, itemAmmount):
+    
+    arrays = AddMass(arrayAmount, itemAmmount)
     
     sums = np.sum(arrays, axis=1)
     index = np.argmax(sums)
     
     return arrays[index]
 
+# Евклидово расстояние.
 def EuclidDistance(A, B):
     diff = (A - B)**2
 
     return np.sqrt(sum(diff.ravel()))
 
+# Нахождение расстояния.
 def MinkovDistance(A, B, p, r):
     p = 2
     r = 2
